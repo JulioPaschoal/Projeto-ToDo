5cef53280bc97aebfd48da1745702c638744a03f
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2021-03-10 20:51:03 -0300

    Refatorando o codigo

--- BACKEND/src/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-// CONFIGURANDO BANCO DE DADOS LOCAL \\
-const url = 'mongodb://localhost:27017/todo';
-mongoose.connect(url, { useNewUrlParser: true });
-
-// EXPORTANDO A CONEXAO \\
-module.exports = mongoose;
